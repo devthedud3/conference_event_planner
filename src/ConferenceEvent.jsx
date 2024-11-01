0a0e3f756f4a0f5644c3159b2ac503cbd890dc1d
corey <corey.cushnie@gmail.com>
2024-11-01 14:16:49 -0400

    add meals

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -4,12 +4,15 @@ import TotalCost from "./TotalCost";
 import { useSelector, useDispatch } from "react-redux";
 import { incrementQuantity, decrementQuantity } from "./venueSlice";
 import { incrementAvQuantity, decrementAvQuantity } from "./avSlice";
+import { toggleMealSelection } from "./mealsSlice";
+toggleMealSelection
 
 const ConferenceEvent = () => {
     const [showItems, setShowItems] = useState(false);
     const [numberOfPeople, setNumberOfPeople] = useState(1);
     const venueItems = useSelector((state) => state.venue);
     const avItems = useSelector((state) => state.av);
+    const mealsItems = useSelector((state) => state.meals);
     const dispatch = useDispatch();
     const remainingAuditoriumQuantity = 3 - venueItems.find(item => item.name === "Auditorium Hall (Capacity:200)").quantity;
 
@@ -40,6 +43,7 @@ const ConferenceEvent = () => {
     };
 
     const handleMealSelection = (index) => {
+        dispatch(toggleMealSelection(index))
 
     };
 
@@ -52,11 +56,23 @@ const ConferenceEvent = () => {
     const ItemsDisplay = ({ items }) => {
 
     };
-    const calculateTotalCost = (data) => {
+    const calculateTotalCost = (selection) => {
         let totalCost = 0;
-        data.forEach((item) => {
-            totalCost += item.cost * item.quantity;
-        });
+        if (selection === 'venue'){
+            venueItems.forEach((item) => {
+                totalCost += item.cost * item.quantity;
+            });
+        }
+        if (selection === 'av'){
+            avItems.forEach((item) => {
+                totalCost += item.cost * item.quantity;
+            });
+        }
+        if (selection === 'meals'){
+            venueItems.forEach((item) => {
+                totalCost += item.cost * item.selected;
+            });
+        }
 
         return totalCost;
     };
@@ -96,6 +112,7 @@ const ConferenceEvent = () => {
                                     <h1>Venue Room Selection</h1>
                                 </div>
                                 <div className="venue_selection">
+
                                     {venueItems.map((item, index) => (
                                         <div className="venue_main" key={index}>
                                             <div className="img">
@@ -191,10 +208,25 @@ const ConferenceEvent = () => {
                                 </div>
 
                                 <div className="input-container venue_selection">
-
+                                    <label htmlFor="numberOfPeople"><h3>Number of People:</h3></label>
+                                    <input type="number" className="input_box5" id="numberOfPeople" value={numberOfPeople}
+                                        onChange={(e) => setNumberOfPeople(parseInt(e.target.value))}
+                                        min="1"
+                                    />
                                 </div>
                                 <div className="meal_selection">
-
+                                    {mealsItems.map((item, index) => (
+                                        <div className="meal_item" key={index} style={{ padding: 15 }}>
+                                            <div className="inner">
+                                                <input type="checkbox" id={`meal_${index}`}
+                                                    checked={item.selected}
+                                                    onChange={() => handleMealSelection(index)}
+                                                />
+                                                <label htmlFor={`meal_${index}`}> {item.name} </label>
+                                            </div>
+                                            <div className="meal_cost">${item.cost}</div>
+                                        </div>
+                                    ))}
                                 </div>
                                 <div className="total_cost">Total Cost: </div>
 
