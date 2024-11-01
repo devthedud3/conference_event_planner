0a28052f0781d95f84af060331bfe83125b1ba5e
corey <corey.cushnie@gmail.com>
2024-11-01 14:30:58 -0400

    add meals

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -43,7 +43,14 @@ const ConferenceEvent = () => {
     };
 
     const handleMealSelection = (index) => {
-        dispatch(toggleMealSelection(index))
+        const item = mealsItems[index];
+    if (item.selected && item.type === "mealForPeople") {
+        const newNumberOfPeople = item.selected ? numberOfPeople : 0;
+        dispatch(toggleMealSelection(index, newNumberOfPeople));
+    }
+    else {
+        dispatch(toggleMealSelection(index));
+    }
 
     };
 
